17e145fdbe7314b0aff96cf0708c6d24bdddcccc
KhrysCS <148722222+KhrysCS@users.noreply.github.com>
2025-04-22 12:32:44 -0500

    M9-L23 Selenium: Page Factory
    M9-L23 Selenium: Page Factory
    
    Modified BasePage,LoginPage and LoginTests to use Page factory and fluent interfaces.

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,28 +2,40 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage {
     public LoginPage(WebDriver givenDriver) {
         super(givenDriver);
     }
-    By emailField = By.cssSelector("input[type='email']");
-    By passwordField = By.cssSelector("input[type='password']");
-    By submitBtn = By.cssSelector("button[type='submit']");
+    @FindBy(css = "input[type='email']") WebElement emailField;
+    @FindBy(css = "input[type='password']") WebElement passwordField;
+    @FindBy(css = "button[type='submit']") WebElement submitBtnLocator;
+    //By emailField = By.cssSelector("input[type='email']");
+  //  By passwordField = By.cssSelector("input[type='password']");
+    //By submitBtn = By.cssSelector("button[type='submit']");
 
-    public void provideEmail(String email){
-        findElement(emailField).sendKeys(email);
+    public LoginPage provideEmail(String email){
+       // findElement(emailField).sendKeys(email);
+        emailField.sendKeys(email);
+        return this;
     }
-    public void providePassword(String password){
-        findElement(passwordField).sendKeys(password);
+    public LoginPage providePassword(String password){
+        //findElement(passwordField).sendKeys(password);
+        passwordField.sendKeys(password);
+        return this;
     }
-    public void clickSubmit(){
-        findElement(submitBtn).click();
+    public LoginPage clickSubmit(){
+      //  findElement(submitBtn).click();
+      submitBtnLocator.click();
+        return this;
     }
-    public void login(){
+    public LoginPage login(){
         provideEmail("khrystal.colon@testpro.io");
         providePassword("t3$t$tudent");
         clickSubmit();
+        return this;
     }
 
     public String getCurrentUrl(){
